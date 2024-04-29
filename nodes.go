44d7c3eaf0d26bcf4f93df140d753a6f92ef1ed1
Pascal Morillon <pascal.morillon@eskemm-numerique.fr>
2024-04-29 13:11:18 +0200

    Add Slurm power saving node states

--- nodes.go
+++ nodes.go
@@ -16,7 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 package main
 
 import (
-	"github.com/prometheus/client_golang/prometheus"
 	"io/ioutil"
 	"log"
 	"os/exec"
@@ -24,19 +23,24 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type NodesMetrics struct {
-	alloc float64
-	comp  float64
-	down  float64
-	drain float64
-	err   float64
-	fail  float64
-	idle  float64
-	maint float64
-	mix   float64
-	resv  float64
+	alloc         float64
+	comp          float64
+	down          float64
+	drain         float64
+	err           float64
+	fail          float64
+	idle          float64
+	maint         float64
+	mix           float64
+	resv          float64
+	powered_down  float64
+	powering_down float64
+	powering_on   float64
 }
 
 func NodesGetMetrics() *NodesMetrics {
@@ -83,26 +87,38 @@ func ParseNodesMetrics(input []byte) *NodesMetrics {
 			maint := regexp.MustCompile(`^maint`)
 			mix := regexp.MustCompile(`^mix`)
 			resv := regexp.MustCompile(`^res`)
+			powered_down := regexp.MustCompile(`^idle\~`)
+			powering_down := regexp.MustCompile(`^idle%`)
+			powering_on := regexp.MustCompile(`^mix#`)
 			switch {
-			case alloc.MatchString(state) == true:
+			case alloc.MatchString(state):
 				nm.alloc += count
-			case comp.MatchString(state) == true:
+			case comp.MatchString(state):
 				nm.comp += count
-			case down.MatchString(state) == true:
+			case down.MatchString(state):
 				nm.down += count
-			case drain.MatchString(state) == true:
+			case drain.MatchString(state):
 				nm.drain += count
-			case fail.MatchString(state) == true:
+			case fail.MatchString(state):
 				nm.fail += count
-			case err.MatchString(state) == true:
+			case err.MatchString(state):
 				nm.err += count
-			case idle.MatchString(state) == true:
+			case idle.MatchString(state):
 				nm.idle += count
-			case maint.MatchString(state) == true:
+				if powered_down.MatchString(state) {
+					nm.powered_down += count
+				}
+				if powering_down.MatchString(state) {
+					nm.powering_down += count
+				}
+			case maint.MatchString(state):
 				nm.maint += count
-			case mix.MatchString(state) == true:
+			case mix.MatchString(state):
 				nm.mix += count
-			case resv.MatchString(state) == true:
+				if powering_on.MatchString(state) {
+					nm.powering_on += count
+				}
+			case resv.MatchString(state):
 				nm.resv += count
 			}
 		}
@@ -135,30 +151,36 @@ func NodesData() []byte {
 
 func NewNodesCollector() *NodesCollector {
 	return &NodesCollector{
-		alloc: prometheus.NewDesc("slurm_nodes_alloc", "Allocated nodes", nil, nil),
-		comp:  prometheus.NewDesc("slurm_nodes_comp", "Completing nodes", nil, nil),
-		down:  prometheus.NewDesc("slurm_nodes_down", "Down nodes", nil, nil),
-		drain: prometheus.NewDesc("slurm_nodes_drain", "Drain nodes", nil, nil),
-		err:   prometheus.NewDesc("slurm_nodes_err", "Error nodes", nil, nil),
-		fail:  prometheus.NewDesc("slurm_nodes_fail", "Fail nodes", nil, nil),
-		idle:  prometheus.NewDesc("slurm_nodes_idle", "Idle nodes", nil, nil),
-		maint: prometheus.NewDesc("slurm_nodes_maint", "Maint nodes", nil, nil),
-		mix:   prometheus.NewDesc("slurm_nodes_mix", "Mix nodes", nil, nil),
-		resv:  prometheus.NewDesc("slurm_nodes_resv", "Reserved nodes", nil, nil),
+		alloc:         prometheus.NewDesc("slurm_nodes_alloc", "Allocated nodes", nil, nil),
+		comp:          prometheus.NewDesc("slurm_nodes_comp", "Completing nodes", nil, nil),
+		down:          prometheus.NewDesc("slurm_nodes_down", "Down nodes", nil, nil),
+		drain:         prometheus.NewDesc("slurm_nodes_drain", "Drain nodes", nil, nil),
+		err:           prometheus.NewDesc("slurm_nodes_err", "Error nodes", nil, nil),
+		fail:          prometheus.NewDesc("slurm_nodes_fail", "Fail nodes", nil, nil),
+		idle:          prometheus.NewDesc("slurm_nodes_idle", "Idle nodes", nil, nil),
+		maint:         prometheus.NewDesc("slurm_nodes_maint", "Maint nodes", nil, nil),
+		mix:           prometheus.NewDesc("slurm_nodes_mix", "Mix nodes", nil, nil),
+		resv:          prometheus.NewDesc("slurm_nodes_resv", "Reserved nodes", nil, nil),
+		powered_down:  prometheus.NewDesc("slurm_nodes_powered_down", "Powered down nodes", nil, nil),
+		powering_down: prometheus.NewDesc("slurm_nodes_powering_down", "Powering down nodes", nil, nil),
+		powering_on:   prometheus.NewDesc("slurm_nodes_powering_on", "Powering on nodes", nil, nil),
 	}
 }
 
 type NodesCollector struct {
-	alloc *prometheus.Desc
-	comp  *prometheus.Desc
-	down  *prometheus.Desc
-	drain *prometheus.Desc
-	err   *prometheus.Desc
-	fail  *prometheus.Desc
-	idle  *prometheus.Desc
-	maint *prometheus.Desc
-	mix   *prometheus.Desc
-	resv  *prometheus.Desc
+	alloc         *prometheus.Desc
+	comp          *prometheus.Desc
+	down          *prometheus.Desc
+	drain         *prometheus.Desc
+	err           *prometheus.Desc
+	fail          *prometheus.Desc
+	idle          *prometheus.Desc
+	maint         *prometheus.Desc
+	mix           *prometheus.Desc
+	resv          *prometheus.Desc
+	powered_down  *prometheus.Desc
+	powering_down *prometheus.Desc
+	powering_on   *prometheus.Desc
 }
 
 // Send all metric descriptions
@@ -173,6 +195,9 @@ func (nc *NodesCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- nc.maint
 	ch <- nc.mix
 	ch <- nc.resv
+	ch <- nc.powered_down
+	ch <- nc.powering_down
+	ch <- nc.powering_on
 }
 func (nc *NodesCollector) Collect(ch chan<- prometheus.Metric) {
 	nm := NodesGetMetrics()
@@ -186,4 +211,7 @@ func (nc *NodesCollector) Collect(ch chan<- prometheus.Metric) {
 	ch <- prometheus.MustNewConstMetric(nc.maint, prometheus.GaugeValue, nm.maint)
 	ch <- prometheus.MustNewConstMetric(nc.mix, prometheus.GaugeValue, nm.mix)
 	ch <- prometheus.MustNewConstMetric(nc.resv, prometheus.GaugeValue, nm.resv)
+	ch <- prometheus.MustNewConstMetric(nc.powered_down, prometheus.GaugeValue, nm.powered_down)
+	ch <- prometheus.MustNewConstMetric(nc.powering_down, prometheus.GaugeValue, nm.powering_down)
+	ch <- prometheus.MustNewConstMetric(nc.powering_on, prometheus.GaugeValue, nm.powering_on)
 }
